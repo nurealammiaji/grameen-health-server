6ee11a0030634540888d06cd6b762780d02fb23c
nurealammiaji <nurealam.miaji@gmail.com>
2024-08-16 21:40:03 +0600

    http server created

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
+const http = require("http");
 const express = require("express");
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const app = express();
+const { jwtRoutes } = require("./routes/jwtRoutes");
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
+app.use("/api", jwtRoutes);
 
 const port = process.env.PORT || 5000;
 
@@ -15,6 +17,8 @@ app.get("/", (req, res) => {
     res.send("Grameen Health Server is running ...");
 })
 
-app.listen(port, () => {
+const server = http.createServer(app);
+
+server.listen(port, () => {
     console.log(`Grameen Health Server is running on port: ${port}`);
 })
