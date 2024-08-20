997a518fa0f4114bc80faa7497d12e2d9bdbf980
nurealammiaji <nurealam.miaji@gmail.com>
2024-08-20 22:08:25 +0600

    mysql db connection added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql");
 const app = express();
 const mainRoutes = require("./routes/mainRoutes");
 
@@ -12,12 +13,26 @@ app.use(cors());
 // Routes
 app.use("/api", mainRoutes);
 
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+const database = mysql.createConnection({
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+})
+
+database.connect((err) => {
+    if (err) {
+        console.log("MySQL Error: ", err);
+    }
+    console.log("MySQL DB Connected Successfully");
+})
+
 app.get("/", (req, res) => {
     res.send("Grameen Health Server is running ...");
 })
-
-const port = process.env.PORT || 5000;
-const server = http.createServer(app);
 
 server.listen(port, () => {
     console.log(`Grameen Health Server is running on port: ${port}`);
