ef210bb17b4f9eeaf79bc5f61d704065e7dad6a4
nurealammiaji <nurealam.miaji@gmail.com>
2024-08-23 03:57:29 +0600

    test get method added

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -5,11 +5,19 @@ const testRoutes = express.Router();
 testRoutes.post("/test", (req, res) => {
     const { name, email } = req.body;
     console.log(name, email)
-    const query = 'INSERT INTO users (name, email) VALUES (?, ?)';
+    const query = 'INSERT INTO test (name, email) VALUES (?, ?)';
     database.query(query, [name, email], (err, result) => {
         if (err) throw err;
         res.status(200).send('User added successfully!');
     });
 })
+
+testRoutes.get('/test', (req, res) => {
+    const query = 'SELECT * FROM test';
+    database.query(query, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 
 module.exports = testRoutes;
