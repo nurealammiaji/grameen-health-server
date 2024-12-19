0cf468dd01429130446f970774b2e910fd3df5ed
nurealammiaji <nurealam.miaji@gmail.com>
2024-12-19 22:04:08 +0600

    allRoutes modified

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -9,6 +9,7 @@ const shopRoutes = require("./shopRoutes");
 const carouselRoutes = require("./carouselRoutes");
 const categoryRoutes = require("./categoryRoutes");
 const subCategoryRoutes = require("./subCategoryRoutes");
+const campaignRoutes = require("./campaignRoutes");
 const reviewRoutes = require("./reviewRoutes");
 const searchRoutes = require("./searchRoutes");
 
@@ -21,6 +22,7 @@ allRoutes.use(shopRoutes);
 allRoutes.use(carouselRoutes);
 allRoutes.use(categoryRoutes);
 allRoutes.use(subCategoryRoutes);
+allRoutes.use(campaignRoutes);
 allRoutes.use(reviewRoutes);
 allRoutes.use(searchRoutes);
 
--- routes/campaignRoutes.js
+++ routes/campaignRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const campaignRoutes = express.Router();
 const { createCampaign, updateCampaign, getAllCampaigns, deleteCampaign, getSingleCampaign, deleteCampaigns } = require('../controllers/campaignController');
 const upload = require('../middlewares/uploadMiddleware');
-const campaignRoutes = express.Router();
 
 campaignRoutes.post('/campaigns/create', upload.single('image'), createCampaign);
 campaignRoutes.put('/campaigns/update/:id', upload.single('image'), updateCampaign);
